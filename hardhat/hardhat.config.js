568c38f07b1bdcaa2c80ff52ed2a577ee4dfe7b6
Martha Staus <mjstaus@gmail.com>
2022-09-12 14:32:53 -0700

    setup: Setup hardhat config file

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,35 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.9",
+ module.exports = {
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    polygon: {
+      url: process.env.INFURA_POLYGON_API_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    mumbai: {
+      url: process.env.INFURA_MUMBAI_API_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: `${process.env.POLYGONSCAN_API_KEY}`
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
 };
